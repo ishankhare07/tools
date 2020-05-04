3da339bdfedb5d3f7dee5145a137a01d4b9f6bf1
Saptanto Sindu <saptanto.sindu@gmail.com>
2020-05-04 13:00:57 +0700

    Add random graph generator to generate service default
    Issue-id: UTOPIA-69

---
+++ isotope/convert/pkg/graph/random_graph_generator.go
@@ -0,0 +1,28 @@
+package graph
+
+import (
+	"math/rand"
+	"time"
+)
+
+func GenerateRandomServiceGraph(numberOfService int, requestSize int, responseSize int, listOfClusters []string, ingressGatewayEndpoint string) ServiceGraph {
+	serviceGraph := new(ServiceGraph)
+	serviceGraph.Defaults = generateServiceDefaults(listOfClusters, ingressGatewayEndpoint)
+
+	return *serviceGraph
+}
+
+func generateServiceDefaults(listOfClusters []string, ingressGatewayEndpoint string) ServiceDefaults {
+	serviceDefaults := new(ServiceDefaults)
+	serviceDefaults.FortioCluster = getRandomCluster(listOfClusters)
+	serviceDefaults.IngressGatewayEndpoint = ingressGatewayEndpoint
+
+	return *serviceDefaults
+}
+
+func getRandomCluster(listOfClusters []string) string {
+	rand.Seed(time.Now().UnixNano())
+	min := 0
+	max := len(listOfClusters)
+	return listOfClusters[rand.Intn(max - min) + min]
+}
